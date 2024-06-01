7f5b7b7fe288600f5fb2ddd16294d5f12235c011
alexander <alexander.caelan.rogers@gmail.com>
2024-05-31 20:54:10 -0400

    create register method

--- src/main/java/net/alexander/tutorialmod/item/ModItems.java
+++ src/main/java/net/alexander/tutorialmod/item/ModItems.java
@@ -2,10 +2,15 @@ package net.alexander.tutorialmod.item;
 
 import net.alexander.tutorialmod.TutorialMod;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
