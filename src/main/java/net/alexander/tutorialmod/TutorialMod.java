b6dfb8b3d73446af509d66b45b23c72bb1a379e2
alexander <alexander.caelan.rogers@gmail.com>
2024-05-31 21:05:27 -0400

    add sapphire to creative mod tab

--- src/main/java/net/alexander/tutorialmod/TutorialMod.java
+++ src/main/java/net/alexander/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.alexander.tutorialmod;
 
 import com.mojang.logging.LogUtils;
 import net.alexander.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -37,7 +38,9 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
