b6d737264ba49da9391d543c7dfe27757da99530
alexander <alexander.caelan.rogers@gmail.com>
2024-05-31 21:32:28 -0400

    create deferred register

--- src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
@@ -1,4 +1,11 @@
 package net.alexander.tutorialmod.item;
 
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.registries.DeferredRegister;
+
 public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
 }
