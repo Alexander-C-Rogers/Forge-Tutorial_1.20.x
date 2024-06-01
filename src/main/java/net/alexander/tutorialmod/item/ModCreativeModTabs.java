8653be1494a9fb2cccf4d97df8de2349304966b8
alexander <alexander.caelan.rogers@gmail.com>
2024-05-31 21:33:46 -0400

    create register method

--- src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
@@ -3,9 +3,14 @@ package net.alexander.tutorialmod.item;
 import net.alexander.tutorialmod.TutorialMod;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 
 public class ModCreativeModTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
