ae2a9f3fb09cae8359ee941d489787090a7f9749
alexander <alexander.caelan.rogers@gmail.com>
2024-06-01 12:23:41 -0400

    add registerBlockItem method

--- src/main/java/net/alexander/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/alexander/tutorialmod/block/ModBlocks.java
@@ -1,14 +1,22 @@
 package net.alexander.tutorialmod.block;
 
 import net.alexander.tutorialmod.TutorialMod;
+import net.alexander.tutorialmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name,RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
