7ee5e7c9b1f5c853b93aabc6bc471313ca4b4858
alexander <alexander.caelan.rogers@gmail.com>
2024-06-01 12:10:53 -0400

    create deferred register

--- src/main/java/net/alexander/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/alexander/tutorialmod/block/ModBlocks.java
@@ -1,4 +1,11 @@
 package net.alexander.tutorialmod.block;
 
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 }
