941a2bc9273a932e6877b27af539660b2e11d850
alexander <alexander.caelan.rogers@gmail.com>
2024-06-02 15:10:22 -0400

    add nether sapphire ore to tutorial tab

--- src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/alexander/tutorialmod/item/ModCreativeModTabs.java
@@ -26,6 +26,7 @@ public class ModCreativeModTabs {
 
                         pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                     })
                     .build());
 
