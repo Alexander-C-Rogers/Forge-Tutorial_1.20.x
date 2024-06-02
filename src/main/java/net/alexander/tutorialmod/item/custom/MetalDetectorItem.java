cec4a1abbe9bce26139a71eb217ce9f7cc532b0a
alexander <alexander.caelan.rogers@gmail.com>
2024-06-02 19:23:27 -0400

    create useOn method

--- src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,9 +1,23 @@
 package net.alexander.tutorialmod.item.custom;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(!pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+        }
+        return InteractionResult.SUCCESS;
     }
 }
