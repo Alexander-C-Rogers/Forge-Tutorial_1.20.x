fdc90db3b2ff40fcb5d3a801b51ed0fddbbb7d2b
alexander <alexander.caelan.rogers@gmail.com>
2024-06-02 19:38:22 -0400

    create outputValuableCoordinates method

--- src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,10 +1,13 @@
 package net.alexander.tutorialmod.item.custom;
 
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -24,11 +27,19 @@ public class MetalDetectorItem extends Item {
                 BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
                 
                 if(isValuableBlock(state)) {
+                    outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
+                    foundBlock = true;
 
+                    break;
                 }
             }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
+        player.sendSystemMessage(Component.literal("Found" + I18n.get(block.getDescriptionId()) + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + "," + blockPos.getZ() + ")"));
     }
 
     private boolean isValuableBlock(BlockState state) {
