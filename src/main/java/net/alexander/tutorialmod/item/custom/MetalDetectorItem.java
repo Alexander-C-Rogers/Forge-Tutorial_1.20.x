87a3dc58ab7ae3201a88b5afd5e0f638933e60b5
alexander <alexander.caelan.rogers@gmail.com>
2024-06-02 19:28:39 -0400

    create isValuableBlock method

--- src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/MetalDetectorItem.java
@@ -5,6 +5,8 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -17,7 +19,19 @@ public class MetalDetectorItem extends Item {
             BlockPos positionClicked = pContext.getClickedPos();
             Player player = pContext.getPlayer();
             boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
+                
+                if(isValuableBlock(state)) {
+
+                }
+            }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
     }
 }
