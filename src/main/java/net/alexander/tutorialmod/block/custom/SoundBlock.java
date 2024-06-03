72fc7452596f07b010b200c09461258aed6c8c15
alexander <alexander.caelan.rogers@gmail.com>
2024-06-02 20:08:12 -0400

    create SoundBlock class

---
+++ src/main/java/net/alexander/tutorialmod/block/custom/SoundBlock.java
@@ -0,0 +1,9 @@
+package net.alexander.tutorialmod.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class SoundBlock extends Block {
+    public SoundBlock(Properties pProperties) {
+        super(pProperties);
+    }
+}
