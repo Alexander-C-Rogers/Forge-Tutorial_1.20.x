8503965c9697ab6ad4261365ce4f25ac0141f9e8
alexander <alexander.caelan.rogers@gmail.com>
2024-06-04 09:18:45 -0400

    create strawberry food item

--- src/main/java/net/alexander/tutorialmod/item/ModFoods.java
+++ src/main/java/net/alexander/tutorialmod/item/ModFoods.java
@@ -1,4 +1,11 @@
 package net.alexander.tutorialmod.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
 public class ModFoods {
+    public static FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
+            .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED,
+                    200), 0.1f).build();
 }
