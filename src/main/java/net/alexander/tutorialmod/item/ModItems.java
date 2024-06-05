a0c8e8112c2f8c4c4ea7d77aecf93cccc8e8257b
alexander <alexander.caelan.rogers@gmail.com>
2024-06-05 13:45:36 -0400

    register pine cone

--- src/main/java/net/alexander/tutorialmod/item/ModItems.java
+++ src/main/java/net/alexander/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.alexander.tutorialmod.item;
 
 import net.alexander.tutorialmod.TutorialMod;
+import net.alexander.tutorialmod.item.custom.FuelItem;
 import net.alexander.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -12,17 +13,19 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
-    /*mod items*/
+    /*Mod Items*/
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
 
-    /*custom mod item*/
+    /*Custom Mod Items*/
     public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
             () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> PINE_CONE = ITEMS.register("pine_cone",
+            () -> new FuelItem(new Item.Properties(), 400));
 
-    /*mod food item*/
+    /*Mod Food Item*/
     public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry",
             () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
 
