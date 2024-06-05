6380f724ac43dd17546069e165a93f68411f6544
alexander <alexander.caelan.rogers@gmail.com>
2024-06-05 13:39:44 -0400

    overide getBurnTime method

--- src/main/java/net/alexander/tutorialmod/item/custom/FuelItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/FuelItem.java
@@ -1,6 +1,9 @@
 package net.alexander.tutorialmod.item.custom;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import org.jetbrains.annotations.Nullable;
 
 public class FuelItem extends Item {
     private int burnTime = 0;
@@ -8,5 +11,10 @@ public class FuelItem extends Item {
     public FuelItem(Properties pProperties, int burnTime) {
         super(pProperties);
         this.burnTime = burnTime;
+    }
+
+    @Override
+    public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
+        return this.burnTime;
     }
 }
