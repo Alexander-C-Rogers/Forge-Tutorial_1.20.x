aebe24d55f83cb518c33c55560a2de5149767021
alexander <alexander.caelan.rogers@gmail.com>
2024-06-05 13:35:45 -0400

    create custom fuel item

---
+++ src/main/java/net/alexander/tutorialmod/item/custom/FuelItem.java
@@ -0,0 +1,12 @@
+package net.alexander.tutorialmod.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class FuelItem extends Item {
+    private int burnTime = 0;
+
+    public FuelItem(Properties pProperties) {
+        super(pProperties);
+        this.burnTime = burnTime;
+    }
+}
