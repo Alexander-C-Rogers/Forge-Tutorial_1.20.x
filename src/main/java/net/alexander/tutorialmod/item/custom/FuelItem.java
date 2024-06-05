90f5c5cb94374f02436ad30d6a4eca4f51223645
alexander <alexander.caelan.rogers@gmail.com>
2024-06-05 13:37:07 -0400

    add missing parameter

--- src/main/java/net/alexander/tutorialmod/item/custom/FuelItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/FuelItem.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.Item;
 public class FuelItem extends Item {
     private int burnTime = 0;
 
-    public FuelItem(Properties pProperties) {
+    public FuelItem(Properties pProperties, int burnTime) {
         super(pProperties);
         this.burnTime = burnTime;
     }
