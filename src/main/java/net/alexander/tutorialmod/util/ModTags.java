887e50bf249d7c55d638f16a138d235313647d39
alexander <alexander.caelan.rogers@gmail.com>
2024-06-15 18:49:12 -0400

    create custom block tag

--- src/main/java/net/alexander/tutorialmod/util/ModTags.java
+++ src/main/java/net/alexander/tutorialmod/util/ModTags.java
@@ -11,6 +11,8 @@ import net.minecraft.world.level.block.Block;
 public class ModTags {
     /*represents block tags*/
     public static class Blocks {
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
         }
