7a2059bbb983f5637d43581f5b9a2f7ab1a60819
alexander <alexander.caelan.rogers@gmail.com>
2024-06-15 18:45:55 -0400

    create Items class

--- src/main/java/net/alexander/tutorialmod/util/ModTags.java
+++ src/main/java/net/alexander/tutorialmod/util/ModTags.java
@@ -3,7 +3,9 @@ package net.alexander.tutorialmod.util;
 import net.alexander.tutorialmod.TutorialMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
@@ -11,6 +13,13 @@ public class ModTags {
     public static class Blocks {
         private static TagKey<Block> tag(String name) {
             return BlockTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
+        }
+    }
+
+    /*represents item tags*/
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
         }
     }
 }
