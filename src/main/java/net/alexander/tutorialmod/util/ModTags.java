5df9f28e9a74fdbbc6ed594d6c40822dffd08cb2
alexander <alexander.caelan.rogers@gmail.com>
2024-06-15 18:42:40 -0400

    create blocks class

--- src/main/java/net/alexander/tutorialmod/util/ModTags.java
+++ src/main/java/net/alexander/tutorialmod/util/ModTags.java
@@ -1,4 +1,16 @@
 package net.alexander.tutorialmod.util;
 
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
 public class ModTags {
+    /*represents block tags*/
+    public static class Blocks {
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(TutorialMod.MOD_ID, name));
+        }
+    }
 }
