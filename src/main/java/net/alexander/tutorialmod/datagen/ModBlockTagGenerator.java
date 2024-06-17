1403fe0ac4d85fb981d4a9d233b1f897f90647e4
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 11:59:12 -0400

    add mineable with pickaxe tag to addTags method

--- src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -5,6 +5,7 @@ import net.alexander.tutorialmod.block.ModBlocks;
 import net.alexander.tutorialmod.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -22,5 +23,14 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
     protected void addTags(HolderLookup.Provider pProvider) {
         this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
                 .add(ModBlocks.SAPPHIRE_ORE.get()).addTag(Tags.Blocks.ORES);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                        ModBlocks.SOUND_BLOCK.get());
     }
 }
