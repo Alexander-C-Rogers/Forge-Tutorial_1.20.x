f733fbc41d9b1c342eb982ae87b59d0c0276ba5d
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 12:13:20 -0400

    add custom method

--- src/main/java/net/alexander/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModItemModelProvider.java
@@ -2,8 +2,12 @@ package net.alexander.tutorialmod.datagen;
 
 import net.alexander.tutorialmod.TutorialMod;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItemModelProvider extends ItemModelProvider {
     public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
@@ -13,5 +17,11 @@ public class ModItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
 
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TutorialMod.MOD_ID,"item/" + item.getId().getPath()));
     }
 }
