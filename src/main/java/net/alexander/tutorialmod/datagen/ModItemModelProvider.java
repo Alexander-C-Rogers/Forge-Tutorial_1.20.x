62c1085c3f14397d3cf30d69d40ffe73f1e10d15
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 12:08:57 -0400

    implement methods

--- src/main/java/net/alexander/tutorialmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModItemModelProvider.java
@@ -1,4 +1,17 @@
 package net.alexander.tutorialmod.datagen;
 
-public class ModItemModelProvider {
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+
+    }
 }
