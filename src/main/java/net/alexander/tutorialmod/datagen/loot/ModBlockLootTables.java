4ca947baca832df0ebe5d3d3eb28002fe3ff02b6
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 12:59:47 -0400

    add deepslate sapphire ore to generate method

--- src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
@@ -33,6 +33,8 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         /*mod ores*/
         this.add(ModBlocks.SAPPHIRE_ORE.get(),
                 block -> createCopperLikeOreDrops(ModBlocks.SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+        this.add(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                block -> createCopperLikeOreDrops(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
     }
 
     protected LootTable.Builder createCopperLikeOreDrops(Block pBLock, Item item) {
