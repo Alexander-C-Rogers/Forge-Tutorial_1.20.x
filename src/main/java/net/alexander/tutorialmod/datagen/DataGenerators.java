f9fa9b9a990b106f1cae1e8f2081037272598b50
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 18:33:00 -0400

    add EventBusSubscriber

--- src/main/java/net/alexander/tutorialmod/datagen/DataGenerators.java
+++ src/main/java/net/alexander/tutorialmod/datagen/DataGenerators.java
@@ -1,4 +1,8 @@
 package net.alexander.tutorialmod.datagen;
 
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
 }
