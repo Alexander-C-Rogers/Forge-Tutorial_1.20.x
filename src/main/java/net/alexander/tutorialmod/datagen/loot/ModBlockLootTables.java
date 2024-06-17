b012333db67fc7b9aa1662e6f46b068a07ab6f60
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 12:37:46 -0400

    implement methods for ModBlockLootTables class

--- src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
@@ -1,4 +1,19 @@
 package net.alexander.tutorialmod.datagen.loot;
 
-public class ModBlockLootTables {
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+
+import java.util.Set;
+
+public class ModBlockLootTables extends BlockLootSubProvider {
+    protected ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags());
+    }
+
+    @Override
+    protected void generate() {
+
+    }
 }
