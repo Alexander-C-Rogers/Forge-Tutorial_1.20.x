5c792793f82a074d049f6fe96aa8b839364232f6
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 12:42:11 -0400

    fixed mistake

--- src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
@@ -2,16 +2,14 @@ package net.alexander.tutorialmod.datagen.loot;
 
 import net.alexander.tutorialmod.block.ModBlocks;
 import net.minecraft.data.loot.BlockLootSubProvider;
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
-    protected ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
+    protected ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
