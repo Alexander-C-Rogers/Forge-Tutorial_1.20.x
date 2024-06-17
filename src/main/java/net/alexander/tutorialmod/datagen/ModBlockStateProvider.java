054573e69a93a03954e4e057f8f40017895b3711
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 11:28:21 -0400

    add custom method

--- src/main/java/net/alexander/tutorialmod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModBlockStateProvider.java
@@ -2,8 +2,10 @@ package net.alexander.tutorialmod.datagen;
 
 import net.alexander.tutorialmod.TutorialMod;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
@@ -13,5 +15,9 @@ public class ModBlockStateProvider extends BlockStateProvider {
     @Override
     protected void registerStatesAndModels() {
 
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
     }
 }
