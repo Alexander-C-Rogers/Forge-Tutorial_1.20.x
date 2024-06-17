c050005a90f07f28027961fd583e25dff469c33c
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 13:03:10 -0400

    add end stone sapphire ore to generate method

--- src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/alexander/tutorialmod/datagen/loot/ModBlockLootTables.java
@@ -37,6 +37,8 @@ public class ModBlockLootTables extends BlockLootSubProvider {
                 block -> createCopperLikeOreDrops(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
         this.add(ModBlocks.NETHER_SAPPHIRE_ORE.get(),
                 block -> createCopperLikeOreDrops(ModBlocks.NETHER_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
+        this.add(ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                block -> createCopperLikeOreDrops(ModBlocks.END_STONE_SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
     }
 
     protected LootTable.Builder createCopperLikeOreDrops(Block pBLock, Item item) {
