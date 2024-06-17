c14b532615fd04c47f4f1f6da1c18b61639f2365
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 11:26:30 -0400

    create BlockStateProvider class

---
+++ src/main/java/net/alexander/tutorialmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,17 @@
+package net.alexander.tutorialmod.datagen;
+
+import net.alexander.tutorialmod.TutorialMod;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TutorialMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+
+    }
+}
