26d2c1f2e178bca6350af5c7088ca3e09109b2b9
alexander <alexander.caelan.rogers@gmail.com>
2024-06-17 11:54:59 -0400

    add custom tag to addTags method

--- src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -1,8 +1,11 @@
 package net.alexander.tutorialmod.datagen;
 
 import net.alexander.tutorialmod.TutorialMod;
+import net.alexander.tutorialmod.block.ModBlocks;
+import net.alexander.tutorialmod.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +20,7 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get()).addTag(Tags.Blocks.ORES);
     }
 }
