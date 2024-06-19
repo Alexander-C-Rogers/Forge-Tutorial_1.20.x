42eaae18134d80319467aae784207228a8998c7f
alexander <alexander.caelan.rogers@gmail.com>
2024-06-19 19:50:06 -0400

    add sapphire wall to Walls tag

--- src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/alexander/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -49,5 +49,7 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.SAPPHIRE_FENCE.get());
         this.tag(BlockTags.FENCE_GATES)
                 .add(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+        this.tag(BlockTags.WALLS)
+                .add(ModBlocks.SAPPHIRE_WALL.get());
     }
 }
