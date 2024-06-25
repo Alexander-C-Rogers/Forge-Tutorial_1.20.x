3867b9dc5c63738518b17f68b596acd145253eb5
alexander <alexander.caelan.rogers@gmail.com>
2024-06-25 18:58:37 -0400

    implement ArmorMaterial methods

--- src/main/java/net/alexander/tutorialmod/item/ModArmorMaterials.java
+++ src/main/java/net/alexander/tutorialmod/item/ModArmorMaterials.java
@@ -1,4 +1,50 @@
 package net.alexander.tutorialmod.item;
 
-public enum ModArmorMaterials {
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    ;
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type pType) {
+        return 0;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type pType) {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return 0;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
 }
