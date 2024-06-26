1860af2864ec31b4fd4ba610a47c31b806a36d86
alexander <alexander.caelan.rogers@gmail.com>
2024-06-26 11:45:51 -0400

    create ModArmorItem class

---
+++ src/main/java/net/alexander/tutorialmod/item/custom/ModArmorItem.java
@@ -0,0 +1,10 @@
+package net.alexander.tutorialmod.item.custom;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class ModArmorItem extends ArmorItem {
+    public ModArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
+        super(pMaterial, pType, pProperties);
+    }
+}
