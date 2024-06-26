6974bcb41cf9a526206da4a37e5d62b32a30e2e5
alexander <alexander.caelan.rogers@gmail.com>
2024-06-26 11:54:16 -0400

    create a new map

--- src/main/java/net/alexander/tutorialmod/item/custom/ModArmorItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/ModArmorItem.java
@@ -1,9 +1,20 @@
 package net.alexander.tutorialmod.item.custom;
 
+import com.google.common.collect.ImmutableMap;
+import net.alexander.tutorialmod.item.ModArmorMaterials;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 
+import java.util.Map;
+
 public class ModArmorItem extends ArmorItem {
+    private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
+                    .put(ModArmorMaterials.SAPPHIRE, new MobEffectInstance(MobEffects.NIGHT_VISION, 200, 1,
+                            false,false, true)).build();
+
     public ModArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
         super(pMaterial, pType, pProperties);
     }
