665f837f87d083453f4d971816f77b404e1d3d13
alexander <alexander.caelan.rogers@gmail.com>
2024-06-26 11:57:42 -0400

    add the onArmorTick method

--- src/main/java/net/alexander/tutorialmod/item/custom/ModArmorItem.java
+++ src/main/java/net/alexander/tutorialmod/item/custom/ModArmorItem.java
@@ -4,18 +4,30 @@ import com.google.common.collect.ImmutableMap;
 import net.alexander.tutorialmod.item.ModArmorMaterials;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 import java.util.Map;
 
 public class ModArmorItem extends ArmorItem {
     private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_TO_EFFECT_MAP =
             (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
-                    .put(ModArmorMaterials.SAPPHIRE, new MobEffectInstance(MobEffects.NIGHT_VISION, 200, 1,
-                            false,false, true)).build();
+                    .put(ModArmorMaterials.SAPPHIRE, new MobEffectInstance(MobEffects.NIGHT_VISION, 200,
+                            1, false,false, true)).build();
 
     public ModArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
         super(pMaterial, pType, pProperties);
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, Level world, Player player) {
+        if(!world.isClientSide()) {
+            if(hasFullSuitOfArmorOn(player)) {
+                evaluateArmorEffects(player);
+            }
+        }
     }
 }
